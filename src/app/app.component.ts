89db3985fa1ae191d56337106051dd281374c05d
ViralSir <viralsir2018@gmail.com>
2024-04-23 10:22:31 +0530

    entry component has been updated.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,15 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {ButtonModule} from "primeng/button";
 import {MenubarModule} from "primeng/menubar";
-import {MenuItem} from "primeng/api";
+import {MenuItem, MessageService} from "primeng/api";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {BrowserModule} from "@angular/platform-browser";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, ButtonModule, MenubarModule],
+  providers:[MessageService],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/entry/entry.component.ts
+++ src/entry/entry.component.ts
@@ -1,12 +1,54 @@
 import { Component } from '@angular/core';
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
+import {CourseService} from "../service/course.service";
+import {Course} from "../model/course";
+import {InputTextModule} from "primeng/inputtext";
+import {InputNumberModule} from "primeng/inputnumber";
+import {ButtonModule} from "primeng/button";
+import {ToastModule} from "primeng/toast";
+import {MessageService} from "primeng/api";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
 @Component({
   selector: 'app-entry',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    InputTextModule,
+    InputNumberModule,
+    ButtonModule,
+    ToastModule,
+
+  ],
   templateUrl: './entry.component.html',
   styleUrl: './entry.component.css'
 })
 export class EntryComponent {
+
+    msg:string="";
+    CourseGroup=new FormGroup({
+       id:new FormControl(""),
+       name:new FormControl("",[Validators.maxLength(50)]),
+      duration:new FormControl(""),
+      fees:new FormControl("",[Validators.min(0) ])
+    })
+
+   constructor(public courseservice:CourseService,private messageService: MessageService) {
+
+   }
+
+   save(){
+
+        let courseform=this.CourseGroup.value;
+        console.log(courseform);
+        let course=new Course(parseInt(<string>courseform.id),<string>courseform.name,<string>courseform.duration,parseInt(<string>courseform.fees));
+        this.courseservice.setCourses(course);
+        this.msg="Record has been saved.";
+        this.CourseGroup.reset();
+        this.messageService.clear();
+        this.messageService.add({ key: 'toast1', severity: 'success', summary: 'Success', detail: this.msg });
+
+   }
+
 
 }
